c88a7b31f488fdd2466a7b6d9bac0ff662e1ee71
Atharv <105142090+atharrvv@users.noreply.github.com>
2026-03-20 16:51:24 +0530

    Update Program.cs

--- dotnet-core-ci-demo/Program.cs
+++ dotnet-core-ci-demo/Program.cs
@@ -1,11 +1,21 @@
-﻿namespace dotnet_core_ci_demo
+using Microsoft.AspNetCore.Builder;
+
+namespace dotnet_core_ci_demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var calc = new Calculator();
-            Console.WriteLine($"3 + 5 = {calc.Add(3, 5)}");
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () =>
+            {
+                var calc = new Calculator();
+                return $"3 + 5 = {calc.Add(3, 5)}";
+            });
+
+            app.Run();
         }
     }
 }
